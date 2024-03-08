2ca222a5370850be25f450666e327b1e51fbdd5c
sAkuraOfficial <mchaohao159@outlook.com>
2024-03-09 01:49:34 +0800

    init repository

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "main.hpp"
+#include <iostream>
+int main()
+{
+    return 0;
+}
+
+sAkura_Chat::sAkura_Chat()
+{
+}
+
+sAkura_Chat::~sAkura_Chat()
+{
+}
---
+++ main.hpp
@@ -0,0 +1,8 @@
+#pragma once
+class sAkura_Chat
+{
+  private:
+  public:
+    sAkura_Chat();
+    ~sAkura_Chat();
+};
